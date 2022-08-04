e571fba2abe86783b048883cd7f6aa1c27c7b810
Joshbosh22 <joshua.lonstein@students.mq.edu.au>
2022-08-04 12:24:37 +1000

    Made some changes to task 3 and completed task 4 of week 2

--- src/Main.java
+++ src/Main.java
@@ -2,32 +2,58 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Main extends JFrame {
-    public static void main(String[] args) throws Exception {
-        Main window = new Main();
-    }
-    class Canvas extends JPanel {
-        public Canvas() {
-            setPreferredSize(new Dimension(720, 720));
-        }
-    }
 
-    @Override
+public static void main(String[] args) throws Exception {
+Main window = new Main();
+}
 
-public void paint(Graphics g){
-for(int i = 10; i < 710; i+=35) {
-    for(int j = 10; j < 710; j+=35) {
-        g.drawRect(i,j,35,35);
-        g.setColor(Color.WHITE);
-        g.fillRect(i, j, 35, 35);
-        g.setColor(Color.BLACK);
+public Main() {
+this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+// created a 20x20 grid
+Grid grid = new Grid(20, 20);
+this.setContentPane(grid);
+this.pack();
+this.setVisible(true);
+}
+
+public class Cell extends JPanel {
+Color fillColor;
+Color borderColor;
+
+public Cell(int width, int height) {
+setPreferredSize(new Dimension(width, height));
+fillColor = Color.WHITE;
+borderColor = Color.BLACK;
+}
+
+@Override
+
+protected void paintComponent(Graphics g) {
+
+super.paintComponent(g);
+g.setColor(fillColor);
+g.fillRect(0, 0, getWidth(), getHeight());
+g.setColor(borderColor);
+g.drawRect(0, 0, getWidth(), getHeight());
+}
+
+}
+
+public class Grid extends JPanel {
+Cell grid[][];
+public Grid(int rows, int cols) {
+grid = new Cell[rows][cols];
+setLayout(new GridLayout(rows, cols));
+for (int i = 0; i < rows; i++) {
+    for (int j = 0; j < cols; j++) {
+        grid[i][j] = new Cell(35, 35);
+        add(grid[i][j]);   
     }
 }
+setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
+
 }
-    private Main() {
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        Canvas canvas = new Canvas();
-        this.setContentPane(canvas);
-        this.pack();
-        this.setVisible(true);
-    }
+
+}
+
 }
